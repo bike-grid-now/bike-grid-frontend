168ad3b6e8d312c3f1e28ecd10f461ac67c77711
Sam Wight <samuelwight@gmail.com>
2022-08-23 17:37:37 -0500

    Fix some accessibility stuff.

--- src/Card.svelte
+++ src/Card.svelte
@@ -1,73 +1,72 @@
 <script>
-    export let icon, heading, subheading, link, notarget = false;
+  export let icon,
+    heading,
+    subheading,
+    link,
+    notarget = false;
 </script>
 
-<a href={link} target={notarget ? "" : "_blank"}>
-    <div class="card">
-        <div class="top">
-            <span class="material-symbols-outlined">{icon}</span>
-            <h1>{heading}</h1>
-            <p>{subheading}</p>
-        </div>
-        <div class="bottom">
-            <span class="material-symbols-outlined arrow">arrow_circle_right</span>
-        </div>
-    </div>
+<a href={link} target={notarget ? "" : "_blank"} class="card">
+  <div class="top">
+    <span class="material-symbols-outlined">{icon}</span>
+    <h1>{heading}</h1>
+    <p>{subheading}</p>
+  </div>
+  <div class="bottom">
+    <span class="material-symbols-outlined arrow">arrow_circle_right</span>
+  </div>
 </a>
 
 <style>
-    a {
-        text-decoration: none;
-    }
+  a {
+    text-decoration: none;
+    display: block;
+  }
 
-    .card {
-        height: 100%;
-        position: relative;
-        /* background-color: #fff; */
-        background-color: #1b1a1b;
-        border-radius: 15px;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-        padding: 50px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-        grid-gap: 25px;
-        border: 1px solid rgb(0, 0, 0);
-        text-align: center;
-        cursor: pointer;
+  .card {
+    background-color: #1b1a1b;
+    border-radius: 15px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    grid-gap: 25px;
+    border: 1px solid rgb(0, 0, 0);
+    text-align: center;
 
-        color: white;
+    color: white;
 
-        transition: box-shadow 0.15s ease, background-color 0.15s ease;
+    transition: box-shadow 0.15s ease, background-color 0.15s ease;
+  }
 
-    }
+  .top {
+    display: flex;
+    flex-direction: column;
+    grid-gap: 25px;
+  }
 
-    .top {
-        display: flex;
-        flex-direction: column;
-        grid-gap: 25px;
-    }
+  .card:hover,
+  .card:focus {
+    box-shadow: 0 0px 10px rgba(0, 0, 0, 0.2);
+    background-color: #c70909;
+  }
 
-    .card:hover {
-        box-shadow: 0 0px 10px rgba(0, 0, 0, 0.2);
-        background-color: #c70909;
-    }
+  .card:hover .arrow,
+  .card:focus .arrow {
+    transform: translateX(10px);
+  }
 
-    .card:hover .arrow {
-        transform: translateX(10px);
-    }
+  .card span {
+    font-size: 4rem;
+  }
 
-    .card span {
-        font-size: 4rem;
-    }
+  .card p {
+    line-height: 175%;
+  }
 
-    .card p {
-        line-height: 175%;
-    }
-
-    .arrow {
-        font-size: 2rem;
-        transition: transform 0.2s;
-    }
+  .arrow {
+    font-size: 2rem;
+    transition: transform 0.2s;
+  }
 </style>
