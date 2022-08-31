14b5a3173a407e2cffce7f50efbed043336c1d83
Sam Wight <samuelwight@gmail.com>
2022-08-31 09:38:31 -0500

    Doing stuff

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,10 @@
 import { getEvents } from "$lib/firebase";
+import { headers } from "$lib/caching";
 
 /** @type {import('./$types').PageLoad} */
-export async function load() {
+export async function load({ setHeaders }) {
+  setHeaders(headers);
+
   return {
     events: await getEvents(),
   };
--- src/routes/events/[slug]/+page.server.js
+++ src/routes/events/[slug]/+page.server.js
@@ -1,9 +1,13 @@
 import { error } from "@sveltejs/kit";
 import { getEvent } from "$lib/firebase";
+import { headers } from "$lib/caching";
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ params: { slug } }) {
+export async function load({ params: { slug }, setHeaders }) {
   const event = await getEvent(slug);
+
+  setHeaders(headers);
+
   if (event) {
     return {
       event,
